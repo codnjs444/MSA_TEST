abfcb9f36abdd3e69be291c0d5b1eee380eef691
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-05-23 18:04:17 +0900

    init

---
+++ ms2/src/main/java/com/example/ms2/controller/Ms2Controller.java
@@ -0,0 +1,15 @@
+package com.example.ms2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ms2")
+public class Ms2Controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "ms2 hello";
+    }
+}
