bbaefa7ad8a0f47c03296bf3c90b45495aa0f7f0
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-05-26 10:00:53 +0900

    init

---
+++ ms1/src/main/java/com/example/ms1/controller/Ms1Controller.java
@@ -0,0 +1,15 @@
+package com.example.ms1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ms1")
+public class Ms1Controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "ms1 hello";
+    }
+}
